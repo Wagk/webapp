be91abf2804a058d6e95ff2f519bcb82d52c0702
Pang Tun Jiang <Pang Tun Jiang>
2017-04-29 19:09:20 +0800

    Initializing the database we need

--- webapp.py
+++ webapp.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
-
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -9,6 +8,13 @@ app = Flask(__name__)
 def index():
     return render_template('index.html',
                            title='this is a title')
+
+@app.route('/caregiver/<hash>')
+def display_caregiver(hash):
+    # TODO(pangt): add HTML template for the caregiver,
+    # TODO(pangt): extract caregiver data from database
+    # TODO(pangt): insert caregiver data into HTML template
+    return
 
 if __name__ == "__main__":
     app.run(debug=False)
