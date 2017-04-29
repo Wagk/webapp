c8b1be3a0d72545443bc0bf73bceb4c68bfa02f2
Pang Tun Jiang <Pang Tun Jiang>
2017-04-29 14:34:34 +0800

    Initial commit for the flask webserver

---
+++ webapp.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+@app.route('/index')
+def index():
+    return render_template('index.html',
+                           title='this is a title')
+
+if __name__ == "__main__":
+    app.run(debug=False)
